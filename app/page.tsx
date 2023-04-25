f8423ec90cd0ba7ccc2571274d712da4fd9e31dc
Sandhya Srinivasan <sandy150225@gmail.com>
2023-04-25 20:25:13 +0200

    changed folder name

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import SideBar from './sidebar/SideBar';
+import SideBar from './SideBar/SideBar';
 import AllNotes from './AllNotes/AllNotes';
 
 const inter = Inter({ subsets: ['latin'] });
