0a0d78e5e2218d87a27cee4941ab4ecd6e37f260
Sandhya Srinivasan <sandy150225@gmail.com>
2023-04-25 20:23:56 +0200

    added heading in notes

--- app/AllNotes/AllNotes.tsx
+++ app/AllNotes/AllNotes.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+'use client'
+import React, {FocusEvent, useState} from 'react'
 
-function AllNotes() {
+function AllNotes(): JSX.Element {
+  const [visibility, isVisible] = useState(false)
+
+  function handleOnKeyDown(e: FocusEvent<HTMLTextAreaElement>): void {
+
+  }
   return (
-    <div className='border-[1px] mx-3 p-2'>
-        <p className='mb-3'>Notes</p>
-    <textarea className='w-[100%] rounded-md border-[2px] border-gray-300 h-[7rem] resize-none' placeholder={'New Note...'} />
+    <div className=' mx-3 border-gray-300 border-[2px] rounded-md p-0'>
+    <textarea className='w-[100%] rounded-md border-[0px] rounded-b-none border-gray-300 h-[2rem] border-b-0 resize-none mb-[-5px] focus:outline-none focis-visible:border-none hidden' placeholder={'Heading'} onFocus={handleOnKeyDown} />
+    <textarea className='w-[100%] rounded-md border-[0px] rounded-t-none border-gray-300 h-[7rem]  border-t-0 resize-none focus:outline-none' placeholder={'New Note...'} onFocus={handleOnKeyDown} />
     </div>
   )
 }
--- app/NavBar/NavBar.tsx
+++ app/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <nav className=' bg-yellow-400 p-2'>Scribble Pad</nav>
+    <nav className=' bg-yellow-400 p-2 text-white font-bold text-[1.3rem]'>Scribble Pad</nav>
   )
 }
 
