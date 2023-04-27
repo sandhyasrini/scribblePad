e6578802888e25a34a76afb3c6204f37ca20c8bd
Sandhya Srinivasan <sandy150225@gmail.com>
2023-04-27 18:16:27 +0200

    changed package manager to pnpm

--- app/AllNotes/AllNotes.tsx
+++ app/AllNotes/AllNotes.tsx
@@ -1,16 +1,26 @@
 'use client'
-import React, {FocusEvent, useState} from 'react'
-
+import React, {FocusEvent, RefObject, useRef, useState} from 'react'
+import {MdOutlineDone} from 'react-icons/md'
+import {IoMdClose} from 'react-icons/io'
 function AllNotes(): JSX.Element {
   const [visibility, isVisible] = useState(false)
+  const textRef = useRef<HTMLTextAreaElement>()
 
   function handleOnKeyDown(e: FocusEvent<HTMLTextAreaElement>): void {
+    isVisible(true)
+  }
 
+  function clearNotes() : void {
+    
   }
   return (
-    <div className=' mx-3 border-gray-300 border-[2px] rounded-md p-0'>
-    <textarea className='w-[100%] rounded-md border-[0px] rounded-b-none border-gray-300 h-[2rem] border-b-0 resize-none mb-[-5px] focus:outline-none focis-visible:border-none hidden' placeholder={'Heading'} onFocus={handleOnKeyDown} />
-    <textarea className='w-[100%] rounded-md border-[0px] rounded-t-none border-gray-300 h-[7rem]  border-t-0 resize-none focus:outline-none' placeholder={'New Note...'} onFocus={handleOnKeyDown} />
+    <div className={`mx-3 border-gray-300 border-[2px] rounded-md p-0`}>
+    <textarea ref={textRef as RefObject<HTMLTextAreaElement>} className={`w-[100%] rounded-md border-[0px] rounded-b-none border-gray-300 ${visibility ? 'h-[2rem]' : 'h-[9rem]'} border-b-0 resize-none mb-[-5px] focus:outline-none focis-visible:border-none font-bold`} placeholder={'Heading'} onFocus={handleOnKeyDown}/>
+    <textarea className={`${visibility ? 'block' : 'hidden'} w-[100%] rounded-md border-[0px] rounded-t-none border-gray-300 h-[7rem]  border-t-0 resize-none focus:outline-none`} placeholder={'Content...'}  />
+    <div className='flex justify-end'>
+    <MdOutlineDone height={'1rem'} className='cursor-pointer'/>
+    <IoMdClose className='cursor-pointer'/>
+    </div>
     </div>
   )
 }
